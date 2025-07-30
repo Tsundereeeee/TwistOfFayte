e9c9876bb102580bf221fa73af7124858eac73b2
OhKannaDuh <uimokd@gmail.com>
2025-07-30 16:17:15 +0100

    Fix

---
+++ TwistOfFayte/Commands/ConfigCommand.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Ocelot.Commands;
+using Ocelot.Modules;
+using TwistOfFayte.Windows;
+
+namespace TwistOfFayte.Commands;
+
+[OcelotCommand]
+public class ConfigCommand(Plugin plugin) : OcelotCommand
+{
+    protected override string Command {
+        get => "/tofconfig";
+    }
+
+    protected override string Description {
+        get => "Opens the Twist of Fayte ui";
+    }
+
+    protected override IReadOnlyList<string> Aliases { get; set; } = [
+        "/tofc",
+    ];
+
+    public override void Execute(string command, string arguments)
+    {
+        plugin.Windows.GetWindow<ConfigWindow>().Toggle();
+    }
+}
---
+++ TwistOfFayte/Commands/MainCommand.cs
@@ -0,0 +1,28 @@
+﻿using Ocelot.Commands;
+using Ocelot.Modules;
+using TwistOfFayte.Windows;
+
+namespace TwistOfFayte.Commands;
+
+[OcelotCommand]
+public class MainCommand(Plugin plugin) : OcelotCommand
+{
+    protected override string Command {
+        get => "/tof";
+    }
+
+    protected override string Description {
+        get => "Opens the Twist of Fayte ui";
+    }
+
+    public override void Execute(string command, string arguments)
+    {
+        if (arguments.Trim() == "config" || arguments.Trim() == "c")
+        {
+            new ConfigCommand(plugin).Execute(command, arguments);
+            return;
+        }
+
+        plugin.Windows.GetWindow<MainWindow>().Toggle();
+    }
+}
